2850cade2ce9a64658427127d4258fa2f6f52404
Aditi Shukla <bt23ece037@iiitn.ac.in>
2026-03-04 19:18:47 +0530

    Update tlc.v

--- tlc.v
+++ tlc.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 14.07.2023 15:11:04
-// Design Name: 
-// Module Name: traffic_light_controller
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module traffic_light_controller(
     input clk,
     input rst,
